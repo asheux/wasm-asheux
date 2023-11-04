f8b82389721ba25718dd77620bb61b0536e88759
asheux <brian.mboya@protonmail.com>
2023-11-04 03:01:00 +0300

    fix routing problem

--- src/lib.rs
+++ src/lib.rs
@@ -55,16 +55,18 @@ impl Main {
         }
     }
 
-    pub fn handle_route(&self) -> JsValue {
+    pub fn handle_route(&self, _id: u8) -> JsValue {
         // Handling routes: return the specific route using clicks
         let route: &str = &self.get_route();
         let dict_instance = Dictionary::new();
+        let _mapped_route = format!("/articles/{_id}");
         log!("Current route: {}", route);
+        log!("Mapped current route: {}", _mapped_route);
         match route {
             "/" => dict_instance.get_tags(),
             "/about" => dict_instance.get_about(),
             "/projects" => dict_instance.get_projects(),
-            "/article" => dict_instance.get_article(),
+            mapped_route if mapped_route == route => String::new().into(),
             _ => "Page not found".to_string().into(),
         }
     }
@@ -81,15 +83,6 @@ impl Dictionary {
             name: String::new(),
             tag: String::new(),
         }
-    }
-
-    // TODO: Add database functionality
-    pub fn get_article(&self) -> JsValue {
-        let json_data = serde_json::to_string(&vec![Dictionary {
-            name: "Article page not implemented yet! Stay tuned.".to_string(),
-            tag: "None".to_string()
-        }]).unwrap();
-        JsValue::from_str(&json_data)
     }
 
     // TODO: Add database functionality
@@ -122,22 +115,32 @@ impl Dictionary {
             "Uninformed search in Artificial Intelligence",
             "Encoding logic in Artificial Intelligence",
             "Encoding logic in AI using Theorem Proving",
-            "Could a meme make mind?",
+            "PE",
             "Is it?",
             "The Real",
+            "Could a meme make mind?",
             "F**k red & blue. I want the green pill",
-            "The Prison"
+            "The Prison",
+            "The Hidden World",
+            "Love and The Universe",
+            "The City",
+            "The Universe Within",
         ].into_iter().map(|i| i.to_string()).collect();
         let tags: Vec<String> = vec![
-            "Programming,ArtificialIntelligence",
-            "Programming,ArtificialIntelligence",
-            "Programming,ArtificialIntelligence",
-            "Programming,ArtificialIntelligence",
-            "Poetry,Consciousness",
-            "Poetry,Consciousness,BrainScience",
-            "Poetry,Consciousness",
-            "Poetry,Love",
-            "Poetry,Consciousness,Escape",
+            "1,Programming,AI",
+            "2,Programming,AI",
+            "3,Programming,AI",
+            "4,Programming,AI",
+            "0,PE",
+            "5,Poetry,Consciousness,Mind,AI",
+            "6,Poetry,Consciousness,Imagination",
+            "7,Consciousness,Poetry,AI,Memes,Richard Dawkins",
+            "8,Poetry,Love,Consciousness",
+            "9,Poetry,Consciousness,Escape,Mind",
+            "10,Poetry,Imagination",
+            "11,Love,Poetry,Imagination",
+            "12,AI,Dreams,Mind,Imagination",
+            "13,Imagination,Poetry",
         ].into_iter().map(|i| i.to_string()).collect();
         let dictionaries: Vec<Dictionary> = names
             .into_iter()
@@ -146,7 +149,6 @@ impl Dictionary {
             .collect();
 
         let json_data = serde_json::to_string(&dictionaries).unwrap();
-        log!("{}", json_data);
         JsValue::from_str(&json_data)
     }
 }
