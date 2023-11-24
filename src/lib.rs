f07f11647a167b033bd320aadecfcfec9c83a4c7
asheux <brian.mboya@protonmail.com>
2023-11-25 01:35:56 +0300

    problem found

--- src/lib.rs
+++ src/lib.rs
@@ -138,7 +138,7 @@ impl Crawler {
     }
 
     pub async fn fetch(&mut self, url: String) {
-        let proxified = format!("http://54.82.39.43/crawl");
+        let proxified = format!("http://54.82.39.43/crawl?url={}", url);
         let mut tries = 0;
         let mut response = None;
 
